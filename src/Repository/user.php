7ce8c436c9c5a148283a9ecce9ed1375da76225b
Elie-Webforce3 <rene.webdev@gmail.com>
2017-07-18 12:50:50 +0200

    18_juillet_a_13

---
+++ src/Repository/user.php
@@ -0,0 +1,208 @@
+<?php
+/**
+ * Created by Formateur
+ * Date: 07/07/2017
+ * Time: 11:22
+ */
+
+namespace Entity;
+use DateTime;
+
+/**
+ * Class User
+ * @package Entity
+ */
+class User
+{
+    private $prenom;
+    private $nom;
+    private $date_naissance;
+    private $email;
+    private $password;
+    private $adresse;
+    private $complement_adresse;
+    private $code_postal;
+    private $ville;
+    private $tel;
+    private $sexe;
+
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * @param mixed $prenom
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateNaissance()
+    {
+        return $this->date_naissance;
+    }
+
+    /**
+     * @param DateTime $date_naissance
+     */
+    public function setDateNaissance($date_naissance)
+    {
+        $this->date_naissance = $date_naissance;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAdresse()
+    {
+        return $this->adresse;
+    }
+
+    /**
+     * @param mixed $adresse
+     */
+    public function setAdresse($adresse)
+    {
+        $this->adresse = $adresse;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getComplementAdresse()
+    {
+        return $this->complement_adresse;
+    }
+
+    /**
+     * @param mixed $complement_adresse
+     */
+    public function setComplementAdresse($complement_adresse)
+    {
+        $this->complement_adresse = $complement_adresse;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCodePostal()
+    {
+        return $this->code_postal;
+    }
+
+    /**
+     * @param mixed $code_postal
+     */
+    public function setCodePostal($code_postal)
+    {
+        $this->code_postal = $code_postal;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getVille()
+    {
+        return $this->ville;
+    }
+
+    /**
+     * @param mixed $ville
+     */
+    public function setVille($ville)
+    {
+        $this->ville = $ville;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTel()
+    {
+        return $this->tel;
+    }
+
+    /**
+     * @param mixed $tel
+     */
+    public function setTel($tel)
+    {
+        $this->tel = $tel;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSexe()
+    {
+        return $this->sexe;
+    }
+
+    /**
+     * @param mixed $sexe
+     */
+    public function setSexe($sexe)
+    {
+        $this->sexe = $sexe;
+    }
+
+
+
+}
